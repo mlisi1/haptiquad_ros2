ec6837a220b843168e17f0d7150ce723b84d2990
m.lisi1 <m.lisi1@studenti.unipi.it>
2025-03-05 15:30:49 +0100

    now momobs configs can be specified from the launch file

--- momobs_ros2/launch/gazebo_wrapper.launch.py
+++ momobs_ros2/launch/gazebo_wrapper.launch.py
@@ -1,21 +1,22 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 
 from launch import LaunchDescription
 
 def generate_launch_description():
 
-
     self_pkg = get_package_share_directory('momobs_ros2')
-
     observer_config = os.path.join(self_pkg, 'config', 'momobs_gazebo.yaml')
+
+    config_file = DeclareLaunchArgument('config_file', observer_config)
 
     momobs = Node(
         package="momobs_ros2", executable="momobs_gazebo",
         emulate_tty = True,
         # remappings=[('/robot_description', '/fb/floating_base_description')],
-        parameters=[observer_config]
+        parameters=[config_file]
     )  
 
 
--- momobs_ros2/launch/mujoco_wrapper.launch.py
+++ momobs_ros2/launch/mujoco_wrapper.launch.py
@@ -1,6 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 
 from launch import LaunchDescription
 
@@ -11,11 +12,13 @@ def generate_launch_description():
 
     observer_config = os.path.join(self_pkg, 'config', 'momobs_mujoco.yaml')
 
+    config_file = DeclareLaunchArgument('config_file', observer_config)
+
     momobs = Node(
         package="momobs_ros2", executable="momobs_mujoco",
         emulate_tty = True,
         # remappings=[('/robot_description', '/fb/floating_base_description')],
-        parameters=[observer_config],
+        parameters=[config_file],
 
     )  
 
